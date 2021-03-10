6dc59cfef25283fbbc842a3786910534de716966
Olga-Smolianinova <olga.smol2015@gmail.com>
2021-03-10 15:17:38 +0200

    initial settings

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+// Components
+import Feedback from './components/Feedback';
+
+import Draft from './components/Draft'; // для тренировки теории модуля
+
+// Styles
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* Home work */}
+        <Feedback />
+
+        {/* для тренировки теории по модулю components//Draft/Draft.js */}
+        <Draft />
+      </div>
+    );
+  }
+}
+
+export default App;
