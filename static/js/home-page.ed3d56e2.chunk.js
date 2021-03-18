3597f9b8bcf70e52495e0ae60f20d7075d82c9eb
Olga-Smolianinova <olga.smol2015@gmail.com>
2021-03-18 16:37:48 +0200

    Updates

---
+++ static/js/home-page.ed3d56e2.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-practice-react-rest-api"]=this["webpackJsonpgoit-practice-react-rest-api"]||[]).push([[4],{71:function(e,t,i){"use strict";i.r(t),i.d(t,"default",(function(){return a}));i(0);var c=i(1),a=function(){return Object(c.jsx)("div",{className:"HomeView",children:Object(c.jsxs)("h1",{className:"HomeView-title",children:["WELCOME TO HOME PAGE",Object(c.jsx)("span",{role:"img","aria-label":"face emoji",children:"\ud83d\ude0a"})]})})}}}]);
+//# sourceMappingURL=home-page.ed3d56e2.chunk.js.map
